cbb0a6ab3aa9f7e0843831c67aeee8c35e14753b
Ron Kosova <ron.kosova@gmail.com>
2023-07-12 11:24:40 +0300

    Added Header component and Header component CSS

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='gradient-text'>
+            Älyjuoma-automaatti Dashboard
+            </h1>
+        </div>
+    )
+}
