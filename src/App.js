a03409c04dccf7b6ab403acb54ed757a173cae8a
Ron Kosova <ron.kosova@gmail.com>
2023-07-12 13:45:16 +0300

    Added test frontend

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Header from './components/Header/Header';
 import { useState, useEffect } from 'react';
 
 function App() {
+  // const [data, setData] = useState([]);
   return (
     <div className="App">
       <Header />
+
+      <h1 className='gradient-text'>
+        TEST FRONTEND
+      </h1>
+
+      <p className='gradient-text'>
+        No data to serve so far. This page is just a test page.
+      </p>
     </div>
   );
 }
